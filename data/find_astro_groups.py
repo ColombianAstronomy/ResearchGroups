86a463774ae42135ac8549afbeb194c695d0027f
jngaravitoc <jngaravitoc@email.arizona.edu>
2020-05-12 20:17:09 -1000

    data address

--- data/find_astro_groups.py
+++ data/find_astro_groups.py
@@ -1,9 +1,16 @@
+"""
+Busca grupos de astronomia en los datos de Colciencias 
+tomados de: https://minciencias.gov.co/ciudadano/datosabiertos
+
+
+"""
+
 #!/usr/bin/python3
 import numpy as np
 import pandas as pd
 import sys
 
-# TODO: Buscar key-words:  Espacio y Cosmologia
+# TODO: Buscar key-words:  Espacio, Cosmologia, y Gravitacion
 
 if __name__ == "__main__":
     fname = sys.argv[1]
