dba654f2090f60ccfa1d6fbee950fb751f15a944
jngaravitoc <jngaravitoc@email.arizona.edu>
2020-05-12 20:01:27 -1000

    astro -

--- data/find_astro_groups.py
+++ data/find_astro_groups.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == "__main__":
     fname = sys.argv[1]
     data = pd.read_csv(fname)
-    astro = data['NME_GRUPO_GR'].str.contains('astro') 
+    astro = data['NME_GRUPO_GR'].str.contains('Astro') 
     data[astro].to_csv('ASTRO_'+fname, encoding='utf-8')
