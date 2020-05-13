818b06b021a95ccc5efad4faea0d789fdad9dd77
jngaravitoc <jngaravitoc@email.arizona.edu>
2020-05-12 19:59:02 -1000

    Grupos de astronomia registrados en Colciencias 2019

---
+++ data/find_astro_groups.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import numpy as np
+import pandas as pd
+import sys
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+    data = pd.read_csv(fname)
+    astro = data['NME_GRUPO_GR'].str.contains('astro') 
+    data[astro].to_csv('ASTRO_'+fname, encoding='utf-8')
